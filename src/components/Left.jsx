fcd1c880261444d5148907549eedc3c4be87d782
henok8515 <henokegezew44@gmail.com>
2023-04-24 00:38:24 +0300

    feat: building the ui

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -1,29 +1,54 @@
 import React from "react";
+import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { Typography } from "@mui/material";
 
 function Left() {
   return (
     <div
       className="
-      flex text-center
-       justify-between
-    max-w-sm rounded overflow-hidden w-72 shadow-lg items-center bg-slate-400 h-screen flex flex-col justify-between "
+       text-center
+    max-w-sm rounded overflow-hidden w-1/3 shadow-lg items-center  h-screen flex flex-col justify-between "
     >
       <div className="flex-2-500">
-        <h1>Madoo </h1>
+        <Typography className="pb-4 text-blue-700 font-semibold" variant="h3">
+          Madoo
+        </Typography>
 
         <img
-          className="w-full bg-red-500 h-44 w-44 rounded-full  flex items-center justify-center "
-          src="/img/card-top.jpg"
+          className=" object-cover h-44 w-44 rounded-full  flex items-center justify-center "
+          src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
           alt=""
         />
       </div>
-      <div className=" flex-1 flex justify-center px-6 py-4 flex flex-col">
-        <button>Home</button>
-        <button>Employees</button>
-        <button>Home</button>
+      <div className="  flex-1 items-center  w-full  justify-center px-6 py-4 flex flex-col">
+        <button class=" flex justify-between  mb-8 w-4/5 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-none hover:border-transparent rounded">
+          <Typography variant="h6">Home</Typography>
+          <HomeOutlinedIcon clsssName="" />
+        </button>
+        <button class=" flex justify-between bg-transparent w-4/5 mb-8 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-none hover:border-transparent rounded">
+          <Typography variant="h6">Employees</Typography>
+          <PeopleAltOutlinedIcon />
+        </button>
+        <button class=" flex justify-between bg-transparent w-4/5 mb-8 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-none hover:border-transparent rounded">
+          <Typography variant="h6">Payment</Typography>
+          <PeopleAltOutlinedIcon />
+        </button>
+        <button class=" flex justify-between bg-transparent w-4/5 mb-8 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-none hover:border-transparent rounded">
+          <Typography variant="h6">Report</Typography>
+          <PeopleAltOutlinedIcon />
+        </button>
+        <button class=" flex justify-between bg-transparent w-4/5 mb-8 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-none hover:border-transparent rounded">
+          <Typography variant="h6">Settings</Typography>
+          <PeopleAltOutlinedIcon />
+        </button>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        <button>Log out</button>
+      <div className="w-full px-6 pt-4 pb-2">
+        <button className="flex justify-between w-full bg-transparent mb-8 hover:bg-red-700 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-none hover:border-transparent rounded">
+          <LogoutOutlinedIcon />
+          <Typography variant="h6">Log out</Typography>
+        </button>
       </div>
     </div>
   );
